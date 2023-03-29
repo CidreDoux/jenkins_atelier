b923248148ae06906f390ce8dbe9b3b87c6460d5
Alexandre-stoppini <stoppini.alexandre@gmail.com>
2023-03-29 15:13:30 +0200

    appli

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 class main {
     //test
-    public static void main(String[] args) { // push 2
+    public static void main(String[] args) { // push 3
         // Créer un Map pour stocker les informations de carte bancaire et les codes PIN
         Map<String, Integer> cartesEtPins = new HashMap<>();
         cartesEtPins.put("123456789012", 1234);
